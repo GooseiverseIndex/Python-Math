d42c2713c9dc288fc20aa4cc01df6fb30251d48c
Gooseiverse <130167135+GooseiverseIndex@users.noreply.github.com>
2025-02-26 13:20:36 -0600

    Update pythagorean_thereom.py

--- pythagorean_thereom.py
+++ pythagorean_thereom.py
@@ -1,4 +1,5 @@
 import time
+# Assigning variables their numbers
 A = int(input("Please type A, If A is missing please type, miss.\n"))
 B = int(input("Please type B, If A is missing please type, miss.\n"))
 C = int(input("Please type C, If A is missing please type, miss.\n"))
