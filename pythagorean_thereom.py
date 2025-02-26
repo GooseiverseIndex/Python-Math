c91c8e1807ad4004a249e8e6eff0697ae2040136
Gooseiverse <130167135+GooseiverseIndex@users.noreply.github.com>
2025-02-26 13:19:37 -0600

    Update pythagorean_thereom.py

--- pythagorean_thereom.py
+++ pythagorean_thereom.py
@@ -1,1 +1,4 @@
-
+import time
+A = int(input("Please type A, If A is missing please type, miss.\n"))
+B = int(input("Please type B, If A is missing please type, miss.\n"))
+C = int(input("Please type C, If A is missing please type, miss.\n"))
